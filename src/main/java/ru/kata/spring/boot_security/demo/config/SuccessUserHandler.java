d3c035c0628268f6b0cd30c9ad078593d7108273
2.1.3_Spring_HelloWorld <Dmitriyfromrussia@gmail.com>
2024-09-06 02:22:26 +0200

    разобрался с ролями

--- src/main/java/ru/kata/spring/boot_security/demo/config/SuccessUserHandler.java
+++ src/main/java/ru/kata/spring/boot_security/demo/config/SuccessUserHandler.java
@@ -19,9 +19,9 @@ public class SuccessUserHandler implements AuthenticationSuccessHandler {
                                         Authentication authentication) throws IOException {
 
         Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
-        //Set<String> requiredRoles = Set.of("ROLE_ADMIN", "ROLE_USER");
+        Set<String> requiredRoles = Set.of("ROLE_ADMIN", "ROLE_USER");
 
-        if (roles.contains("ROLE_ADMIN") && roles.contains("ROLE_USER")) {
+        if (roles.containsAll(requiredRoles)) { //roles.contains("ROLE_ADMIN") && roles.contains("ROLE_USER" //    второй вариант
             httpServletResponse.sendRedirect("/admin/all-users");
 
         } else if (roles.contains("ROLE_USER")) {
